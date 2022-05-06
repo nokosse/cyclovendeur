ecfbf1f2f882c3e46378f1e2f0c0ad157212fac2
nokosse <kvisouthivong@protonmail.comgit config --global color.difff autogit config --global user.email kvisouthivong@protonmail.com>
2022-05-06 17:16:08 +0200

    Ajout des fichiers php/css

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+session_start();
+require_once("db.php");
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <title>Accueil</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <h1>Page d'accueil</h1>
+</body>
+</html>
